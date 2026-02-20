948032a3da1db4e668b986d2057c05ff2a0bd2b2
Samiksha Shinde <samikshashinde142002@gmail.com>
2026-02-20 17:35:00 +0530

    Header + footer + contact + slider enhancement

--- js/script.js
+++ js/script.js
@@ -56,3 +56,13 @@ message.innerText="Something went wrong.";
 }
 });
 }
+
+// Navbar scroll effect
+window.addEventListener("scroll", function() {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
